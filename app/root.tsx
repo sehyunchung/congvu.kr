0bfd9c6cd3941331c6265080e996054a6c7373ae
sehyunchung <sehyun.chung@icloud.com>
2022-07-24 01:08:09 +0900

    1 file modified

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ export function links() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' };
+  return { title: 'CONG VU' };
 };
 
 export default function App() {
