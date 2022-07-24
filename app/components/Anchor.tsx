c93b63f12e35832f044105f6122d3d30cdc94120
Sehyun Chung <sehyun.chung@icloud.com>
2022-07-24 21:05:56 +0900

    attempt 1

--- app/components/Anchor.tsx
+++ app/components/Anchor.tsx
@@ -1,9 +1,25 @@
 export function Anchor({
   children,
+  className,
   ...props
 }: React.ComponentProps<"a">): React.ReactElement {
   return (
-    <a className="hover:underline" {...props}>
+    <a className={`hover:underline ${className}`} {...props}>
+      {children}
+    </a>
+  );
+}
+
+export function BorderedAnchor({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"a">): React.ReactElement {
+  return (
+    <a
+      className={`flex justify-center align-middle text-white rounded-3xl py-2 px-5 ${className}`}
+      {...props}
+    >
       {children}
     </a>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,50 +1,69 @@
-import { Anchor } from "~/components/Anchor";
+import { LinksFunction } from "@remix-run/node";
+import { BorderedAnchor } from "~/components/Anchor";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "preload",
+      as: "image",
+      href: "/stars.gif",
+    },
+  ];
+};
 
 export default function Index() {
   return (
-    <div className="container p-4 font-serif text-lg">
-      <h1 className="text-3xl font-bold">CONG VU</h1>
-      <div className="h-2" />
-      <ul className="flex gap-4">
-        <li>
-          <Anchor
-            target="_blank"
-            href="mailto:info.congvu@gmail.com"
-            rel="noreferrer"
-          >
-            mail
-          </Anchor>
-        </li>
-        <li>
-          <Anchor
-            target="_blank"
-            href="https://soundcloud.com/cong_vu"
-            rel="noreferrer"
-          >
-            soundcloud
-          </Anchor>
-        </li>
-        <li>
-          <Anchor
-            target="_blank"
-            href="https://instagram.com/cong_vu"
-            rel="noreferrer"
-          >
-            instagram
-          </Anchor>
-        </li>
-        <li>
-          <Anchor
-            target="_blank"
-            href="https://www.youtube.com/channel/UCJE2B7eU86iLhk6p467XcJQ"
-            rel="noreferrer"
-          >
-            youtube
-          </Anchor>
-        </li>
-      </ul>
+    <div
+      className="antialiased h-screen flex flex-col justify-center items-center p-4 font-comic text-lg"
+      style={{ background: 'url("/stars.gif")' }}
+    >
       <div>
-        <img src="" />
+        <h1 className="relative font-serif text-9xl text-white text-center">
+          Cong Vu
+        </h1>
+        <div className="h-8" />
+        <ul className="flex gap-4 flex-wrap justify-center">
+          <li>
+            <BorderedAnchor
+              className="bg-gradient-to-r from-sky-500 to-indigo-500"
+              target="_blank"
+              href="mailto:info.congvu@gmail.com"
+              rel="noreferrer"
+            >
+              mail
+            </BorderedAnchor>
+          </li>
+          <li>
+            <BorderedAnchor
+              className="bg-gradient-to-r from-orange-400 to-red-500"
+              target="_blank"
+              href="https://soundcloud.com/cong_vu"
+              rel="noreferrer"
+            >
+              soundcloud
+            </BorderedAnchor>
+          </li>
+          <li>
+            <BorderedAnchor
+              className="bg-gradient-to-r from-pink-600 to-amber-800"
+              target="_blank"
+              href="https://instagram.com/cong_vu"
+              rel="noreferrer"
+            >
+              instagram
+            </BorderedAnchor>
+          </li>
+          <li>
+            <BorderedAnchor
+              className="bg-gradient-to-r from-red-500 to-red-800"
+              target="_blank"
+              href="https://www.youtube.com/channel/UCJE2B7eU86iLhk6p467XcJQ"
+              rel="noreferrer"
+            >
+              youtube
+            </BorderedAnchor>
+          </li>
+        </ul>
       </div>
     </div>
   );
