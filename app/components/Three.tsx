6a7c3dc46b6784d6138254ea7a8100996a78b4ca
Sehyun Chung <sehyun.chung@icloud.com>
2022-08-01 00:58:22 +0900

    modify animation

--- app/components/Three.tsx
+++ app/components/Three.tsx
@@ -30,7 +30,7 @@ function Controls() {
 function Rig() {
   const camera = useThree((state) => state.camera);
   return useFrame((state) => {
-    camera.position.z = state.clock.getElapsedTime() * -1;
+    camera.position.z = state.clock.getElapsedTime() * -0.001;
   });
 }
 
@@ -41,13 +41,13 @@ export function SkyBg() {
     <Canvas camera={{ position: [0, 0, 20], fov: 90 }}>
       <RemixBridge>
         <ambientLight intensity={1.9} />
-        <pointLight intensity={1.8} position={[0, 0, 3500]} />
+        <pointLight intensity={1.8} position={[0, 100, 500]} />
         <Suspense fallback={null}>
-          <Cloud position={[-24, -2, -35]} speed={0.2} opacity={0.3} />
+          <Cloud position={[-24, -2, -35]} speed={0.4} opacity={0.3} />
           <Cloud position={[4, 2, -15]} speed={0.2} opacity={0.4} />
           <Cloud position={[-4, 2, -10]} speed={0.2} opacity={0.2} />
-          <Cloud position={[4, -2, -5]} speed={0.2} opacity={0.25} />
-          <Cloud position={[8, 2, -100]} speed={0.2} opacity={0.15} />
+          <Cloud position={[4, -2, -5]} speed={0.2} opacity={0.2} />
+          <Cloud position={[8, 2, -100]} speed={0.2} opacity={0.1} />
         </Suspense>
         <Sky
           azimuth={3}
