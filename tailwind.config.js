7999e7b05e4bd851b39f828375852021570e1f6b
Sehyun Chung <sehyun.chung@icloud.com>
2023-03-11 13:11:29 +0900

    N13

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        serif: ['Libre Caslon Display', 'serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
