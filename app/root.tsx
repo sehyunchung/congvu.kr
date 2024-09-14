d757413f6fa7db2be4981f156ae7cd8c738b7658
Sehyun Chung <sehyun.chung@icloud.com>
2024-09-14 17:34:19 +0900

    add some content

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-[#e8e8e8]">
         {children}
         <ScrollRestoration />
         <Scripts />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '24': 'repeat(24, 1fr)',
+      },
+      gridTemplateRows: {
+        '24': 'repeat(24, 1fr)',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
